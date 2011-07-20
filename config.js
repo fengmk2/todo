52f9e5413079d0943057acdd3720b40511a13493
fengmk2 <fengmk2@gmail.com>
2011-07-20 18:25:51 +0800

    add submodule

--- config.js
+++ config.js
@@ -5,7 +5,9 @@ exports.site_name = 'Node TODO';
 exports.site_desc = '';
 exports.session_secret = 'tsoedsosisession_secretonsheecfrxedta';
 
-var db_options = {host: 'localhost',port: 3306,
+var db_options = {
+    host: 'localhost',
+    port: 3306,
     user: 'root',
     password: '123456',
     database: 'todo'
