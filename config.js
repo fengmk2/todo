f3b199e9d5c8768b2cc6e7e5161f92d9be525c62
fengmk2 <fengmk2@gmail.com>
2011-08-17 15:42:04 +0800

    兼容旧mysql接口

--- config.js
+++ config.js
@@ -13,10 +13,16 @@ var db_options = {
     database: 'todo'
 };
 
-var db = exports.db = new require('mysql').createClient(db_options);
-//db.connect(function(err) {
-//    if(err) {
-//        console.error('connect db ' + db.host + ' error: ' + err);
-//        process.exit();
-//    }
-//});
+var mysql = new require('mysql'), db = null;
+if(mysql.createClient) {
+    db = mysql.createClient(db_options);
+} else {
+    db = new mysql.Client(db_options);
+    db.connect(function(err) {
+        if(err) {
+            console.error('connect db ' + db.host + ' error: ' + err);
+            process.exit();
+        }
+    });
+}
+exports.db = db;
